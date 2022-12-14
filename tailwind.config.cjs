c3a72995d61000c98f39d9a8f58ce1eebda7fddb
Linnea Visser <anna56c1@stud.kea.dk>
2022-12-14 17:46:17 +0100

    Product view page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,15 @@ module.exports = {
 				"header-y": "var(--header-y-padding)",
 				"burger-padding": "var(--space-s-m)",
 				"breadcrumbs-gap": "var(--space-3xs)",
-				"breadcrumbs-x": "var(--product-view-padding-x)",
-				"breadcrumbs-y": "var(--breadcrumbs-padding-y)"
+				"breadcrumbs-x": "var(--breadcrumbs-padding-x)",
+				"breadcrumbs-y": "var(--breadcrumbs-padding-y)",
+				"wine-padding-y": "var(--wine-padding-y)",
+				"body-padding-x": "var(--body-padding-x)",
+				"space-m": "var(--space-m)",
+				"space-l": "var(--space-l)",
+				"space-xs": "var(--space-xs)",
+				"space-2xs": "var(--space-2xs)",
+				"space-3xs": "var(--space-3xs)",
 			},
 
 			gridTemplateColumns:{
@@ -23,6 +30,14 @@ module.exports = {
 			colors:{
 				"accent-red-main": "var(--color-accent-red-500)",
 				"burger-blue": "var(--color-accent-blue-100)",
+				"red-wine-primary": "var(--color-red-wine-primary)",
+				"red-wine-secondary": "var(--color-red-wine-secondary)",
+				"white-wine-primary": "var(--color-white-wine-primary)",
+				"white-wine-secondary": "var(--color-white-wine-secondary)",
+				"rose-wine-primary": "var(--color-rose-wine-primary)",
+				"rose-wine-secondary": "var(--color-rose-wine-secondary)",
+				"sparkling-wine-primary": "var(--color-sparkling-wine-primary)",
+				"sparkling-wine-secondary": "var(--color-sparkling-wine-secondary)"
 			}
 
 		},
